cc564ba6407a8e7cb18b10dd4aca344e7b63e53d
Alex Gregori Tomás <reproteq@gmail.com>
2022-08-25 16:18:25 +0200

    Update auth.php

--- lang/zh_TW/auth.php
+++ lang/zh_TW/auth.php
@@ -12,7 +12,7 @@
 */
 
 return [
-    'failed'   => '使用者名稱或密碼錯誤。',
-    'password' => '密碼錯誤。',
-    'throttle' => '嘗試登入太多次，請在 :seconds 秒後再試。',
+    'failed'   => 'failed',
+    'password' => 'password',
+    'throttle' => 'throttle :seconds seconds',
 ];
